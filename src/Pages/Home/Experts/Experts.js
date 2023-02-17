2b3fc236c138c9254ef82a217b6030ae6e780497
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2023-02-18 02:34:18 +0600

    minimum resposive with bootstarp

---
+++ src/Pages/Home/Experts/Experts.js
@@ -0,0 +1,31 @@
+import React from "react";
+import expert_1 from "../../../images/experts/expert-1.jpg";
+import expert_2 from "../../../images/experts/expert-2.jpg";
+import expert_3 from "../../../images/experts/expert-3.jpg";
+import expert_4 from "../../../images/experts/expert-4.jpg";
+import expert_5 from "../../../images/experts/expert-5.jpg";
+import expert_6 from "../../../images/experts/expert-6.png";
+import Expert from "../Expert/Expert";
+
+const Experts = () => {
+  const experts = [
+    { id: 1, name: "Mahfuz", img: expert_1 },
+    { id: 2, name: "Sajeeb", img: expert_2 },
+    { id: 3, name: "Avik", img: expert_3 },
+    { id: 4, name: "Deben", img: expert_4 },
+    { id: 5, name: "Raz", img: expert_5 },
+    { id: 6, name: "Sifat", img: expert_6 },
+  ];
+  return (
+    <div className="container">
+      <h2 className="text-primary text-center">Our Experts.</h2>
+      <div className="row">
+        {experts.map((expert) => (
+          <Expert key={expert.id} expert={expert}></Expert>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Experts;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <h2>this is header</h2>
+      <h2>This is header</h2>
     </header>
   );
 };
