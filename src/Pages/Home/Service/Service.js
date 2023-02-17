7c2b104b62fb82ac39cf0ff960bd5a32ce6dcf76
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2023-02-18 01:48:57 +0600

    service with style

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,11 +1,17 @@
 import React from "react";
+import "./Service.css";
 
 const Service = ({ service }) => {
   const { name, img, description, price } = service;
   return (
-    <div>
+    <div className="service">
       <img src={img} alt="" />
       <h2>{name}</h2>
+      <p>Price: {price}</p>
+      <p>
+        <small>{description}</small>
+      </p>
+      <button>Book It</button>
     </div>
   );
 };
