b68273ded22b71db06a727720c6bef583af37f05
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2023-02-19 13:17:03 +0600

    got url parameter in serviceDetails

--- src/Pages/ServiceDtails/ServiceDetails.js
+++ src/Pages/ServiceDtails/ServiceDetails.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ServiceDetails = () => {
+  const { serviceId } = useParams();
   return (
     <div>
-      <h2>Welcome to service details.</h2>
+      <h2>Welcome to service details: {serviceId}</h2>
     </div>
   );
 };
