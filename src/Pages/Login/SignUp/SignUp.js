2d0ee7e89d2739c5e6bb0984362b165f92eb6646
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2023-03-03 11:40:35 +0600

    register page added

--- src/Pages/Login/SignUp/SignUp.js
+++ src/Pages/Login/SignUp/SignUp.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const navigate = useNavigate();
+
+  const navigateSingIn = () => {
+    navigate("/signin");
+  };
   return (
     <div className=" mt-3 container w-50 mx-auto">
       <h2>Please Register</h2>
@@ -23,6 +29,16 @@ const SignUp = () => {
         <Button variant="primary" type="submit">
           Register
         </Button>
+        <p className="mt-3">
+          Already have account?{" "}
+          <span
+            style={{ cursor: "pointer" }}
+            className="text-danger"
+            onClick={navigateSingIn}
+          >
+            Please SingIn
+          </span>
+        </p>
       </Form>
     </div>
   );
