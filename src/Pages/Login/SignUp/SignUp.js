3bb203c40f431dca48fc67ee8362f1a9e57223dc
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2023-03-03 11:54:07 +0600

    got from value

--- src/Pages/Login/SignUp/SignUp.js
+++ src/Pages/Login/SignUp/SignUp.js
@@ -8,15 +8,33 @@ const SignUp = () => {
   const navigateSingIn = () => {
     navigate("/signin");
   };
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(name, email, password);
+  };
   return (
     <div className=" mt-3 container w-50 mx-auto">
       <h2>Please Register</h2>
-      <Form>
+      <Form onSubmit={handleRegister}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Your Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Your Name" required />
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Enter Your Name"
+            required
+          />
           <Form.Label className="mt-3">Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" required />
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            required
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -24,7 +42,12 @@ const SignUp = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" required />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Register
